871335231f7c5c46694b9532b7d6c85ab974c066
ashermanli <ashermanli@gmail.com>
2021-03-27 21:27:20 -0600

    Added Notification component and styles

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -2,18 +2,23 @@ import React, {useState, useEffect} from 'react'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
+import Notification from './components/Notification'
+import './index.css'
 import axios from 'axios'
 import personService from './services/person'
 import { __SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED } from 'react'
 
 const App = () => {
-  const [persons, setPersons] = useState([])
 
+  //intitialize the state
+    const [persons, setPersons] = useState([])
     const[newName, setNewName] = useState('add a new name')
     const[newNumber, setNewNumber] = useState('add a phone number')
     const [filter, setFilter] = useState('')
     const [filteredPerson, setFilteredPerson] = useState('')
+    const [notification, setNotification] = useState('')
 
+    //get data from the server
     useEffect(() => {
       personService.getAll().then( initList => setPersons(initList))
     }, [])
@@ -21,6 +26,7 @@ const App = () => {
 
 
 
+  //will handle adding and updating entries
   const handleSubmit = (e)=>{
     e.preventDefault()
 
@@ -29,18 +35,30 @@ const App = () => {
       number: newNumber
     }
 
+    //if already exists, grab the person object
     const pers = persons.find(p => p.name === newName)
-
+    //change number if different from saved
     const changedPers = {...pers, number: newNumber}
 
     if(persons.some((person) => person.name === newName)){
 
       console.log(persons.filter(person => person.name === newName));
       
+      //ask the user if they wish to update the current contact
       if(window.confirm(`Do you want to update the number for ${newName}`)=== true){
-        personService.update(pers.id, changedPers).then(newList => {
-        setPersons(persons.map(person => person.name !== newName ? person: newList))
+        personService.update(pers.id, changedPers).then(newEntry => {
+        setPersons(persons.map(person => person.name !== newName ? person: newEntry))
         })
+
+        //set message on success
+        setNotification({message: `Entry for ${newName} has been updated`})
+        //clear message after some time
+        setTimeout(() => {
+          setNotification({name:null, status: 'neutral'})
+        }, 5000)
+
+        setNewName('')
+        setNewNumber('')
         return
       }
       else return;
@@ -56,7 +74,13 @@ const App = () => {
       return
     }
 
+    //add the new person to the server
     personService.create(newPerson).then(newList => setPersons(persons.concat(newList)))
+    //set message on success, then clear after some time
+    setNotification({message: `${newName} has been added`})
+    setTimeout(() => {
+      setNotification(null)
+    }, 5000)
     setNewName('')
     setNewNumber('')
 
@@ -68,23 +92,24 @@ const App = () => {
     personService.remove(id).then(del => {
       setPersons(persons.filter(per => per.id !== id))
     }) 
+    .catch(error => {
+      //if the user does not exist, display an error
+      setNotification({error: `"${name}" has already been removed from the server`})
+
+      //update the list so it doesnt show the already removed person
+      setPersons(persons.filter(person => person.name !== name))
+
+      //clear the message after some time
+      setTimeout(() => {
+        setNotification(null)
+      }, 5000)
+    })
     
   }
 
-  // const handleUpdate = (name, id, newNumber) => {
-  //   if(persons.filter(person => person.name === name)){
-  //     if(window.confirm(`Do you want to update the number for ${name}`)=== true){
-  //       personService.update(id, newNumber).then(newList => {
-  //        setPersons(persons.map(person => {
-  //          person.id !== id ? person: person.concat({...person, number: newNumber})
-  //        }))
-  //       })
-  //     }
-  //     else return;
-  //   }
-    
-  // }
 
+
+  //functions for dealing with the input fields. 
   const handleNewName = (e) =>{
     console.log(e.target.value)
     setNewName(e.target.value)
@@ -111,6 +136,9 @@ const App = () => {
   return(
     <div>
       <h2>Phonebook</h2>
+      
+      <Notification message={notification ? notification.message? notification.message: notification.error:null} 
+                    className={notification? notification.message? 'success':'error':null} />
     <Filter filter={filter} filteredPerson={filteredPerson} handleFilter={handleFilter} />
     <h3>Add someone new</h3>
     <PersonForm newName = {newName} handleNewName={handleNewName} newNumber={newNumber} handleNewNumber={handleNewNumber} handleSubmit={handleSubmit}/>
