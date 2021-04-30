7eb2e0222fd26a5d1e3ffa8373f69a38a7c82475
ashermanli <ashermanli@gmail.com>
2021-04-29 22:07:14 -0600

    small refactoring

--- phonebook/src/components/Filter.js
+++ phonebook/src/components/Filter.js
@@ -4,7 +4,9 @@ const Filter = ({filter,filteredPerson, handleFilter}) =>{
   return(
   <div className='search-box'>
     <div> <h2>Find:</h2> <input onChange={handleFilter}/> </div>
-    <div className='search-result'>{filter === '' ? '': filteredPerson.map(person => <p key={person.name}>{person.name} {person.number} </p>)}</div>
+    <div className='search-result'>{filter === '' ? '': 
+     filteredPerson.map(person => 
+     <p key={person.name}>{person.name} {person.number} </p>)}</div>
 </div>
 )
 }
