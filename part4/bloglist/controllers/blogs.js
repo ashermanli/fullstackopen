f6dea3e18a5b774ec195392e4852f64d0c48b9c8
ashermanli <ashermanli@gmail.com>
2023-06-26 10:53:21 -0600

    added get and delete by id routes

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -2,31 +2,46 @@ const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
 //Default page
-blogsRouter.get('/', (request, response) => {
-	Blog.find({})
-		.then(blogs => {
-			//console.log(blogs)
-			response.json(blogs)
-		})
+blogsRouter.get('/', async (request, response) => {
+	const blogs = await Blog.find({})
+	response.json(blogs)
 	//response.send('<h1>Hello world test</h1>')
 })
 
-/*blogsRouter.get('/api/blogs', (request, response) => {
-	Blog.find({})
-		.then(blogs => {
-			console.log(blogs)
-			response.json(blogs)
-		})
-})*/
+blogsRouter.get('/:id', async (request, response) => {
+	const blog = await Blog.findById(request.params.id)
 
-blogsRouter.post('/', (request, response) => {
+	if(blog){
+		response.json(blog)
+	}
+	else {
+		response.status(404).end()
+	}
+})
 
-	const blog = new Blog(request.body)
 
-	blog.save()
-		.then(result => {
-			response.status(201).json(result)
-		})
+blogsRouter.post('/', async (request, response) => {
+
+	const body = request.body
+
+	const blog = new Blog({
+		title: body.title,
+		likes: body.likes ? body.likes: 0
+	})
+
+	const savedBlog = await blog.save()
+	response.status(201).json(savedBlog)
+})
+
+blogsRouter.delete('/:id', async (request, response) => {
+	const id = request.params.id
+
+	const result = await Blog.findByIdAndRemove(id)
+
+	console.log(result)
+	console.log('item has been deleted')
+
+	response.status(204).end()
 })
 
 module.exports = blogsRouter
