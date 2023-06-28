f8c458ddf080edc391fb20b8414edea625cab962
ashermanli <ashermanli@gmail.com>
2023-06-27 20:45:56 -0600

    added user schema and user routes

--- part4/bloglist/controllers/users.js
+++ part4/bloglist/controllers/users.js
@@ -5,6 +5,18 @@ const User = require('../models/user')
 usersRouter.post('/', async (request, response) => {
 	const { username, name, password } = request.body
 
+	if(!username){
+		return response.status(401).json({ error: 'missing username' })
+	} else if (username.length < 3) {
+		return response.status(401).json({ error: 'username should be at least 3 characters' })
+	}
+
+	if(!password){
+		return response.status(401).json({ error: 'missing password' })
+	}else if (password.length< 3) {
+		return response.status(401).json({ error: 'password should be at least 3 characters' })
+	}
+
 	const saltRounds = 10
 	const passwordHash = await bcrypt.hash(password,saltRounds)
 
