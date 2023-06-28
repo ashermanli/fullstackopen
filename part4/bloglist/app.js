619dce93254856a2b795c835e88c134080d19e8b
ashermanli <ashermanli@gmail.com>
2023-06-27 20:45:26 -0600

    added login routes

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -6,9 +6,9 @@ const app = express()
 const cors = require('cors')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 const middleware = require('./utils/middleware')
 const mongoose = require('mongoose')
-
 
 
 //Connection string and instance
@@ -20,21 +20,19 @@ mongoose.connect(mongoURL)
 		logger.error('couldnt connect', e)
 	})
 
-
-
 app.use(cors())
 app.use(express.static('build'))
 app.use(express.json())
 app.use(middleware.requestLogger)
+app.use(middleware.tokenExtractor)
 
 //for router to be used, url starts with /api/blogs
 //e.g. localhost:3005/api/blogs
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users',usersRouter)
+app.use('/api/login', loginRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
-
-
 
 module.exports = app
