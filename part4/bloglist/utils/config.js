f6e16c1298f3bdf8ac384172195bbe7523be72d4
ashermanli <ashermanli@gmail.com>
2023-06-29 11:42:45 -0600

    added script and config for monitoring test server

--- part4/bloglist/utils/config.js
+++ part4/bloglist/utils/config.js
@@ -1,7 +1,10 @@
 require('dotenv').config()
 
-const PORT = process.env.PORT
-const MONGODB_URI =  process.env.NODE_ENV === 'test'
+const PORT = process.env.FLAG === 'monitor'
+	? process.env.MONITOR
+	: process.env.PORT
+
+const MONGODB_URI =  process.env.NODE_ENV === 'test' || process.env.FLAG === 'monitor'
 	? process.env.TEST_MONGODB_URI
 	: process.env.MONGODB_URI
 
