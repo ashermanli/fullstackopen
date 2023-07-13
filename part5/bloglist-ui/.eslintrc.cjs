4c1ae8e660f8d72608dfc5cbc475075a96153f36
ashermanli <ashermanli@gmail.com>
2023-07-13 16:33:52 -0600

    added eslint config

--- part5/bloglist-ui/.eslintrc.cjs
+++ part5/bloglist-ui/.eslintrc.cjs
@@ -1,59 +1,60 @@
 /* eslint-env node */
 
 module.exports = {
-  env: { browser: true, es2020: true },
-  extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:react/jsx-runtime",
-    "plugin:react-hooks/recommended",
-    "prettier",
-  ],
-  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
-  settings: { react: { version: "18.2" } },
-  plugins: ["react-refresh", "simple-import-sort"],
-  rules: {
-    "simple-import-sort/imports": "error",
-    "simple-import-sort/exports": "error",
-    "sort-imports": [
-      "error",
-      {
-        ignoreCase: true,
-        ignoreMemberSort: false,
-        memberSyntaxSortOrder: ["none", "single", "multiple", "all"],
-        allowSeparatedGroups: true,
-      },
-    ],
-    "react-refresh/only-export-components": [
-      "warn",
-      { allowConstantExport: true },
-    ],
-  },
-  overrides: [
-    // override "simple-import-sort" config
-    {
-      files: ["*.js", "*.jsx", "*.ts", "*.tsx", "*.cjs", "*.cjsx"],
-      rules: {
-        "simple-import-sort/imports": [
-          "error",
-          {
-            groups: [
-              // Packages `react` related packages come first.
-              ["^react", "^@?\\w"],
-              // Internal packages.
-              ["^(@|components)(/.*|$)"],
-              // Side effect imports.
-              ["^\\u0000"],
-              // Parent imports. Put `..` last.
-              ["^\\.\\.(?!/?$)", "^\\.\\./?$"],
-              // Other relative imports. Put same-folder imports and `.` last.
-              ["^\\./(?=.*/)(?!/?$)", "^\\.(?!/?$)", "^\\./?$"],
-              // Style imports.
-              ["^.+\\.?(css)$"],
-            ],
-          },
-        ],
-      },
-    },
-  ],
-};
+	env: { browser: true, es2020: true, node: true },
+	extends: [
+		'eslint:recommended',
+		'plugin:react/recommended',
+		'plugin:react/jsx-runtime',
+		'plugin:react-hooks/recommended',
+		'prettier',
+	],
+	parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+	settings: { react: { version: '18.2' } },
+	plugins: ['react-refresh', 'simple-import-sort'],
+	rules: {
+		'no-undef': 'error',
+		'simple-import-sort/imports': 'error',
+		'simple-import-sort/exports': 'error',
+		'sort-imports': [
+			'error',
+			{
+				ignoreCase: true,
+				ignoreMemberSort: false,
+				memberSyntaxSortOrder: ['none', 'single', 'multiple', 'all'],
+				allowSeparatedGroups: true,
+			},
+		],
+		'react-refresh/only-export-components': [
+			'warn',
+			{ allowConstantExport: true },
+		],
+	},
+	overrides: [
+		// override "simple-import-sort" config
+		{
+			files: ['*.js', '*.jsx', '*.ts', '*.tsx', '*.cjs', '*.cjsx'],
+			rules: {
+				'simple-import-sort/imports': [
+					'error',
+					{
+						groups: [
+							// Packages `react` related packages come first.
+							['^react', '^@?\\w'],
+							// Internal packages.
+							['^(@|components)(/.*|$)'],
+							// Side effect imports.
+							['^\\u0000'],
+							// Parent imports. Put `..` last.
+							['^\\.\\.(?!/?$)', '^\\.\\./?$'],
+							// Other relative imports. Put same-folder imports and `.` last.
+							['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
+							// Style imports.
+							['^.+\\.?(css)$'],
+						],
+					},
+				],
+			},
+		},
+	],
+}
