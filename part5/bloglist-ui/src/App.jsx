c6a78e93f986a88cf85d02bc4794bdc7715e8835
ashermanli <ashermanli@gmail.com>
2023-07-14 11:20:35 -0600

    added axios and login form

--- part5/bloglist-ui/src/App.jsx
+++ part5/bloglist-ui/src/App.jsx
@@ -1,25 +1,47 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import blogService from '../services/blogs'
 
 import Blog from '../components/Blog'
 import BlogEntry from '../components/BlogEntry'
 
 const App = () => {
-	let [count, setCount] = useState(0)
+	const [blogs, setBlogs] = useState([])
+	const [username, setUsername] = useState('')
+	const [password, setPassword] = useState('')
+
+	useEffect(() => {
+		blogService.getAll().then((blogs) => {
+			setBlogs(blogs)
+		})
+	})
+
+	const handleLogin = (event) => {
+		event.preventDefault()
+		console.log('logging in with', username, password)
+	}
 
 	return (
 		<>
 			<h1>App Body</h1>
-			<Blog>
-				<BlogEntry
-					title={count}
-					handleRemove={() => setCount(count + 1)}
-				></BlogEntry>
-			</Blog>
-			<BlogEntry
-				title={count}
-				handleRemove={() => setCount(count + 1)}
-			></BlogEntry>
-		</>
+			<form onSubmit={handleLogin}>
+				<label htmlFor="username">Username</label>
+				<input
+					type="text"
+					id="username"
+					value={username}
+					onChange={(event) => setUsername(event.target.value)}
+				/>
+				<label htmlFor="password">Password</label>
+				<input
+					type="password"
+					id="password"
+					value={password}
+					onChange={(event) => setPassword(event.target.value)}
+				/>
+				<button type="submit">Login</button>
+			</form>
+			<div></div>
+		-</>
 	)
 }
 
